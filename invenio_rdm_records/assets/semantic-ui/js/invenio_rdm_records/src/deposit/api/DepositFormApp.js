1a407af4cfdfad32b5bcf3ee47007b9530d78464
Brian Ressler <carlofabyss@hotmail.com>
2026-01-31 23:17:56 -0600

    Fix ternary operator for deserialization logic

--- invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/api/DepositFormApp.js
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/api/DepositFormApp.js
@@ -82,9 +82,7 @@ export class DepositFormApp extends Component {
     if (props.errors) {
       // Use the serializer, but ensure the result is 'React-safe'
       const deserialized = recordSerializer.deserializeErrors(props.errors);
-      appConfig.errors = typeof deserialized === "string"
-        ? deserialized 
-        : JSON.stringify(deserialized);
+      appConfig.errors = typeof deserialized === "string" ? deserialized : JSON.stringify(deserialized);
     }
 
     this.store = configureStore(appConfig);
